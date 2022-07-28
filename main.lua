aa3eea75013349bf7592ae1c65d61dc070e19fe3
RooRay <86845749+RooRay@users.noreply.github.com>
2022-07-28 15:05:25 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- Made by RooRay aka PVPPro9575
--- Please check the README on the github page before using this script! github.com/RooRay/Script-Launcher/blob/main/README.md
+-- Please check the README on the github page before using this script! github.com/RooRay/Script-Launcher/blob/prerelease/README.md
 -- This version of the script is version 1.10
 
 -- Instances:
